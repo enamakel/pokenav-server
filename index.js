3be6922eab46969d155604d0bc3aee4dc38a48e8
Steven Enamakel <steven.enamakel@gmail.com>
2016-07-28 01:14:49 +0530

    make parse insecure

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ var api = new ParseServer({
   }
 });
 
+var allowInsecureHTTP = true;
 var dashboard = new ParseDashboard({
   "apps": [
     {
@@ -32,7 +33,7 @@ var dashboard = new ParseDashboard({
       "appName": "PokeNav"
     }
   ]
-});
+}, allowInsecureHTTP);
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
 // javascriptKey, restAPIKey, dotNetKey, clientKey
